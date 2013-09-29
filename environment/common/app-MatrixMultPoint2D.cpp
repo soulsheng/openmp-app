7c93bc612ecc03e4d90c20dc4706c1d24b1e0e0b
soulsheng <soulsheng@qq.com>
2013-09-29 16:44:43 +0800

    三维数据结构 顶点 矩阵

--- environment/common/app-MatrixMultPoint2D.cpp
+++ environment/common/app-MatrixMultPoint2D.cpp
@@ -5,24 +5,6 @@
 
 #include "app-MatrixMultPoint2D.h"
 
-#define ONE_MATRIX_EACH_POINT	0
-
-#define SIZE_SCALE_P			(1<<10)
-#define SIZE_POINT		(1<<10)
-
-#if !ONE_MATRIX_EACH_POINT
-	#define SIZE_SCALE_M	1
-	#define SIZE_MATRIX		(1<<0)
-#else
-	#define SIZE_SCALE_M	SIZE_SCALE_P
-	#define SIZE_MATRIX		SIZE_POINT
-#endif
-
-#define ELEMENT_COUNT_POINT		2
-#define ELEMENT_COUNT_LINE		3
-#define ELEMENT_LENGTH_LINE		3
-
-#define ELEMENT_COUNT_MATIRX	(ELEMENT_COUNT_LINE*ELEMENT_LENGTH_LINE)
 
 
 int CMatrixMultPoint2D::mmp(  bool bMulti )
@@ -48,33 +30,36 @@ void CMatrixMultPoint2D::mmpSerial( )
 void CMatrixMultPoint2D::mmpParallel( )
 {
 #pragma omp parallel for
-	for (int i=0;i<m_nSizePoint;i++)
+	for (int i=0;i<SIZE_HEIGHT;i++)
 	{
-		int *pInOne = m_pIn + i*ELEMENT_COUNT_POINT ;
-		int *pOutOne = m_pOut + i*ELEMENT_COUNT_POINT ;
+		for (int j=0;j<SIZE_WIDTH;j++)
+		{
+			int *pInOne =	m_pIn[i][j] ;
+			int *pOutOne = m_pOut[i][j] ;
 #if ONE_MATRIX_EACH_POINT
-		float *pMatOne = m_pMat + i*ELEMENT_COUNT_MATIRX ;
+			float *pMatOne = m_pMat + i*ELEMENT_COUNT_MATIRX ;
 #else
-		float *pMatOne = m_pMat + m_pIndex[i]*ELEMENT_COUNT_MATIRX ;
+			float (*pMatOne)[ELEMENT_COUNT_LINE][ELEMENT_LENGTH_LINE] = &m_pMat[ m_pIndex[i][j] ] ;
 #endif
 		//kernelElement( pInOne, pOutOne, pMatOne );
-		pOutOne[0] =
-			pMatOne[0*ELEMENT_LENGTH_LINE+0] * pInOne[0] +
-			pMatOne[1*ELEMENT_LENGTH_LINE+0] * pInOne[1] +
-			pMatOne[2*ELEMENT_LENGTH_LINE+0] ;
+			pOutOne[0] =
+				(*pMatOne)[0][0] * pInOne[0] +
+				(*pMatOne)[1][0] * pInOne[1] +
+				(*pMatOne)[2][0] ;
 
-		pOutOne[1] =
-			pMatOne[0*ELEMENT_LENGTH_LINE+1] * pInOne[0] +
-			pMatOne[1*ELEMENT_LENGTH_LINE+1] * pInOne[1] +
-			pMatOne[2*ELEMENT_LENGTH_LINE+1] ;
+			pOutOne[1] =
+				(*pMatOne)[0][1] * pInOne[0] +
+				(*pMatOne)[1][1] * pInOne[1] +
+				(*pMatOne)[2][1] ;
+		}
 	} /*-- End of omp parallel for --*/
 }
 
 void CMatrixMultPoint2D::Init()
 {
-	m_nSizePoint = SIZE_POINT * SIZE_SCALE_P;
+	m_nSizePoint = SIZE_HEIGHT * SIZE_WIDTH;
 	m_nSizeMatrix = SIZE_MATRIX * SIZE_SCALE_M;
-
+#if 0
 	m_pIn=(int *)malloc( m_nSizePoint*sizeof(int) *ELEMENT_COUNT_POINT );
 	m_pMat=(float *)malloc( m_nSizeMatrix*sizeof(float) *ELEMENT_COUNT_MATIRX );
 	m_pOut=(int *)malloc( m_nSizePoint*sizeof(int) *ELEMENT_COUNT_POINT );
@@ -91,15 +76,38 @@ void CMatrixMultPoint2D::Init()
 
 	for (j=0; j<m_nSizePoint; j++)
 		m_pIndex[j] = j%m_nSizeMatrix;
+#else
+	m_pIn = new int[SIZE_HEIGHT][SIZE_WIDTH][ELEMENT_COUNT_POINT];
+	m_pOut = new int[SIZE_HEIGHT][SIZE_WIDTH][ELEMENT_COUNT_POINT];
+	m_pOutRef = new int[SIZE_HEIGHT][SIZE_WIDTH][ELEMENT_COUNT_POINT];
+	
+	m_pIndex = new int[SIZE_HEIGHT][SIZE_WIDTH];
+	
+	m_pMat=new float[m_nSizeMatrix][ELEMENT_COUNT_LINE][ELEMENT_LENGTH_LINE];
+
+	for (int i=0;i<SIZE_HEIGHT;i++)
+		for (int j=0;j<SIZE_WIDTH;j++)
+			for (int k=0;k<ELEMENT_COUNT_POINT;k++)
+				m_pIn[i][j][k] = ((i*SIZE_WIDTH + j)*ELEMENT_COUNT_POINT ) +k;
+
+	for (int i=0; i<m_nSizeMatrix; i++)
+		for (int j=0;j<ELEMENT_COUNT_LINE;j++)
+			for (int k=0;k<ELEMENT_LENGTH_LINE;k++)
+				m_pMat[i][j][k] = (j*ELEMENT_COUNT_LINE + k)/(float)(ELEMENT_LENGTH_LINE*ELEMENT_COUNT_LINE) ;
+
+	for (int i=0;i<SIZE_HEIGHT;i++)
+		for (int j=0;j<SIZE_WIDTH;j++)
+			m_pIndex[i][j] = j%m_nSizeMatrix;
+#endif
 }
 
 void CMatrixMultPoint2D::UnInit()
 {
-	if (m_pIn)	{ free(m_pIn); m_pIn=NULL; }
-	if (m_pMat)	{ free(m_pMat); m_pMat=NULL; }
-	if (m_pOut)	{ free(m_pOut); m_pOut=NULL; }
-	if (m_pOutRef){ free(m_pOutRef); m_pOutRef=NULL; }
-	if (m_pIndex)	{ free(m_pIndex); m_pIndex=NULL; }
+	if (m_pIn)	{ delete[](m_pIn); m_pIn=NULL; }
+	if (m_pMat)	{ delete[](m_pMat); m_pMat=NULL; }
+	if (m_pOut)	{ delete[](m_pOut); m_pOut=NULL; }
+	if (m_pOutRef){ delete[](m_pOutRef); m_pOutRef=NULL; }
+	if (m_pIndex)	{ delete[](m_pIndex); m_pIndex=NULL; }
 }
 
 void CMatrixMultPoint2D::Implement( bool bMulti )
@@ -146,27 +154,30 @@ void CMatrixMultPoint2D::mmpRef()
 
 }
 
-void CMatrixMultPoint2D::kernel( int* pIn, int* pOut, float* pMat, int* pIndex )
+void CMatrixMultPoint2D::kernel( int (*pIn)[SIZE_WIDTH][ELEMENT_COUNT_POINT], int (*pOut)[SIZE_WIDTH][ELEMENT_COUNT_POINT], float(*pMat)[ELEMENT_COUNT_LINE][ELEMENT_LENGTH_LINE], int (*pIndex)[SIZE_WIDTH] )
 {
-	for (int i=0;i<m_nSizePoint;i++)
+	for (int i=0;i<SIZE_HEIGHT;i++)
 	{
-		int *pInOne = pIn + i*ELEMENT_COUNT_POINT ;
-		int *pOutOne = pOut + i*ELEMENT_COUNT_POINT ;
+		for (int j=0;j<SIZE_WIDTH;j++)
+		{
+			int *pInOne =	m_pIn[i][j] ;
+			int *pOutOne = m_pOut[i][j] ;
 #if ONE_MATRIX_EACH_POINT
-		float *pMatOne = m_pMat + i*ELEMENT_COUNT_MATIRX ;
+			float *pMatOne = m_pMat + i*ELEMENT_COUNT_MATIRX ;
 #else
-		float *pMatOne = m_pMat + m_pIndex[i]*ELEMENT_COUNT_MATIRX ;
+			float (*pMatOne)[ELEMENT_COUNT_LINE][ELEMENT_LENGTH_LINE] = &m_pMat[ m_pIndex[i][j] ] ;
 #endif
-		//kernelElement( pInOne, pOutOne, pMatOne );
-		pOutOne[0] =
-			pMatOne[0*ELEMENT_LENGTH_LINE+0] * pInOne[0] +
-			pMatOne[1*ELEMENT_LENGTH_LINE+0] * pInOne[1] +
-			pMatOne[2*ELEMENT_LENGTH_LINE+0] ;
+			//kernelElement( pInOne, pOutOne, pMatOne );
+			pOutOne[0] =
+				(*pMatOne)[0][0] * pInOne[0] +
+				(*pMatOne)[1][0] * pInOne[1] +
+				(*pMatOne)[2][0] ;
 
-		pOutOne[1] =
-			pMatOne[0*ELEMENT_LENGTH_LINE+1] * pInOne[0] +
-			pMatOne[1*ELEMENT_LENGTH_LINE+1] * pInOne[1] +
-			pMatOne[2*ELEMENT_LENGTH_LINE+1] ;
+			pOutOne[1] =
+				(*pMatOne)[0][1] * pInOne[0] +
+				(*pMatOne)[1][1] * pInOne[1] +
+				(*pMatOne)[2][1] ;
+		}
 
 	}
 	
--- environment/common/app-MatrixMultPoint2D.h
+++ environment/common/app-MatrixMultPoint2D.h
@@ -2,6 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ONE_MATRIX_EACH_POINT	0
+
+#define SIZE_WIDTH			(1<<10)
+#define SIZE_HEIGHT		(1<<10)
+
+#if !ONE_MATRIX_EACH_POINT
+#define SIZE_SCALE_M	1
+#define SIZE_MATRIX		(1<<0)
+#else
+#define SIZE_SCALE_M	SIZE_WIDTH
+#define SIZE_MATRIX		SIZE_HEIGHT
+#endif
+
+#define ELEMENT_COUNT_POINT		2
+#define ELEMENT_COUNT_LINE		3
+#define ELEMENT_LENGTH_LINE		3
+
+#define ELEMENT_COUNT_MATIRX	(ELEMENT_COUNT_LINE*ELEMENT_LENGTH_LINE)
+
 class CMatrixMultPoint2D
 {
 public:
@@ -21,13 +40,13 @@ protected:
 
 	void mmpRef( );
 
-	void kernel(int* pIn, int* pOut, float* pMat, int* pIndex);
+	void kernel(int (*pIn)[SIZE_WIDTH][ELEMENT_COUNT_POINT], int (*pOut)[SIZE_WIDTH][ELEMENT_COUNT_POINT], float(*pMat)[ELEMENT_COUNT_LINE][ELEMENT_LENGTH_LINE], int (*pIndex)[SIZE_WIDTH]);
 	void kernelElement(int* pIn, int* pOut, float* pMat);
 
 private:
-	int *m_pIn,*m_pOutRef,*m_pOut;
-	float *m_pMat;
-	int	*m_pIndex;
+	int (*m_pIn)[SIZE_WIDTH][ELEMENT_COUNT_POINT],(*m_pOutRef)[SIZE_WIDTH][ELEMENT_COUNT_POINT],(*m_pOut)[SIZE_WIDTH][ELEMENT_COUNT_POINT];
+	float (*m_pMat)[ELEMENT_COUNT_LINE][ELEMENT_LENGTH_LINE];
+	int	(*m_pIndex)[SIZE_WIDTH];
 	int m_nSizePoint, m_nSizeMatrix;
 	bool m_bMulti;
 };
